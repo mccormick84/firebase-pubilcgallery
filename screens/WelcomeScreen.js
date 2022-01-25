0f50d1e1a868920a69b9b3c85e11e9fe3c3288aa
Kenny <mccormick84@gmail.com>
2022-01-25 19:48:55 +0900

    firebase 회원 정보 등록 기능 추가
    (cherry picked from commit f245722976e1c9332f5efb1cd3bfcd3ced641c44)

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {KeyboardAvoidingView, Platform, StyleSheet, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-<<<<<<< HEAD
 import SetupProfile from '../components/SetupProfile';
-=======
->>>>>>> c9fab224d76f4baf8a4d7422f238767e7b5e6d87
 
 export default function WelcomeScreen() {
   return (
@@ -14,10 +11,7 @@ export default function WelcomeScreen() {
       <SafeAreaView style={styles.block}>
         <Text style={styles.title}>환영합니다!</Text>
         <Text style={styles.description}>프로필을 설정하세요.</Text>
-<<<<<<< HEAD
         <SetupProfile />
-=======
->>>>>>> c9fab224d76f4baf8a4d7422f238767e7b5e6d87
       </SafeAreaView>
     </KeyboardAvoidingView>
   );
