edf78ef77b19d614290e4c6ecf6daa7a5b91ab03
Kenny <mccormick84@gmail.com>
2022-01-28 14:23:31 +0900

    포스트 작성 기능 구현 2

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,30 +1,69 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {useRoute} from '@react-navigation/native';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
+import {useRoute, useNavigation} from '@react-navigation/native';
 import {
   StyleSheet,
   View,
   TextInput,
-  Image,
   useWindowDimensions,
+  Animated,
+  Keyboard,
 } from 'react-native';
+import IconRightButton from '../components/IconRightButton';
 
 export default function UploadScreen() {
   const route = useRoute();
   const {res} = route.params || {};
   const {width} = useWindowDimensions();
+  const animation = useRef(new Animated.Value(width)).current;
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const [description, setDescription] = useState('');
+  const navigation = useNavigation();
+  const onSubmit = useCallback(() => {
+    // 포스트 작성 로직 구현 예정
+  }, []);
+
+  useEffect(() => {
+    const didShow = Keyboard.addListener('keyboardDidShow', () =>
+      setIsKeyboardOpen(true),
+    );
+    const didHide = Keyboard.addListener('keyboardDidHide', () =>
+      setIsKeyboardOpen(false),
+    );
+    return () => {
+      didShow.remove();
+      didHide.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isKeyboardOpen ? 0 : width,
+      useNativeDriver: false,
+      duration: 150,
+      delay: 100,
+    }).start();
+  }, [isKeyboardOpen, width, animation]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <IconRightButton onPress={onSubmit} name={'send'} />,
+    });
+  }, [navigation, onSubmit]);
 
   return (
     <View style={styles.block}>
-      <Image
+      <Animated.Image
         source={{uri: res.assets[0]?.uri}}
-        style={[styles.image, {height: width}]}
+        style={[styles.image, {height: animation}]}
         resizeMode={'cover'}
       />
       <TextInput
         style={styles.input}
         multiline={true}
-        placeholder={'이 사진에 대한 설명을 입력하세용~'}
+        placeholder={'⌨ 이 사진에 대한 설명을 입력하세요️'}
         textAlignVertical={'top'}
+        value={description}
+        onChangeText={setDescription}
       />
     </View>
   );
